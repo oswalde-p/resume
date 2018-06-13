fcf6f27ca061fd10e949ad2404bef01a2607d169
jPursey-UoM <jpursey@student.unimelb.edu.au>
2018-06-14 01:00:12 +1000

    fix background position, add about me content

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,6 @@
 function toContent(){
     var elmnt = document.getElementById("content");
     elmnt.scrollIntoView();
+    var arrow = document.getElementById("scrollSignifier");
+    arrow.style.display = "none";
 }
